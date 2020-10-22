c18f2616aae4be69027bf1b26acf3fc725b8f3cc
Igor Yeremin <igor.yeremin@gmail.com>
2020-10-22 17:28:17 -0400

    Configure 2am plasma with CL drivers

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2736,6 +2736,19 @@
 
 #endif // HAS_L64XX
 
+#if HAS_CLOSEDLOOP_CONFIG
+  #if AXIS_IS_CLOSEDLOOP(X)
+    #define X_ENCODER_PPU 16384.0 / 360
+    #define X_SERIAL_RX_PIN P1_23
+    #define X_SERIAL_TX_PIN P1_22
+  #endif
+  #if AXIS_IS_CLOSEDLOOP(Y)
+    #define Y_ENCODER_PPU 16384.0 / 360
+    #define Y_SERIAL_RX_PIN P1_21
+    #define Y_SERIAL_TX_PIN P1_20
+  #endif
+#endif // HAS_CLOSEDLOOP_CONFIG
+
 // @section i2cbus
 
 //
