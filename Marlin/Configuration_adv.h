1a9d7e3579331d76869b494f0b9b13ac9d6e55b5
Igor Yeremin <igor.yeremin@gmail.com>
2021-02-03 14:36:00 -0500

    Enable USE_CONTROLLER_FAN for cooling stepper drivers

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -371,15 +371,18 @@
  * The fan turns on automatically whenever any driver is enabled and turns
  * off (or reduces to idle speed) shortly after drivers are turned off.
  */
-//#define USE_CONTROLLER_FAN
+#define USE_CONTROLLER_FAN
 #if ENABLED(USE_CONTROLLER_FAN)
-  //#define CONTROLLER_FAN_PIN -1        // Set a custom pin for the controller fan
+  // disable other fan functinos
+  #define FAN_PIN -1
+  #define FAN1_PIN -1
+  #define CONTROLLER_FAN_PIN P2_03        // Set a custom pin for the controller fan
   //#define CONTROLLER_FAN_USE_Z_ONLY    // With this option only the Z axis is considered
   #define CONTROLLERFAN_SPEED_MIN      0 // (0-255) Minimum speed. (If set below this value the fan is turned off.)
   #define CONTROLLERFAN_SPEED_ACTIVE 255 // (0-255) Active speed, used when any motor is enabled
   #define CONTROLLERFAN_SPEED_IDLE     0 // (0-255) Idle speed, used when motors are disabled
   #define CONTROLLERFAN_IDLE_TIME     60 // (seconds) Extra time to keep the fan running after disabling motors
-  //#define CONTROLLER_FAN_EDITABLE      // Enable M710 configurable settings
+  #define CONTROLLER_FAN_EDITABLE        // Enable M710 configurable settings
   #if ENABLED(CONTROLLER_FAN_EDITABLE)
     #define CONTROLLER_FAN_MENU          // Enable the Controller Fan submenu
   #endif
@@ -2880,11 +2883,15 @@
 #define LASER_FEATURE
 #ifdef MINI_MODEL_2AM
 #define SPINDLE_LASER_ENA_PIN P1_31
-#define SPINDLE_LASER_PWM_PIN HEATER_BED_PIN
+#define SPINDLE_LASER_PWM_PIN P2_05
 #else
-#define SPINDLE_LASER_ENA_PIN HEATER_BED_PIN
+#define SPINDLE_LASER_ENA_PIN P2_05
 //#define SPINDLE_LASER_PWM_PIN 2
 #endif
+
+// disable bed heat, using that pin for torch
+#define HEATER_BED_PIN -1
+
 #if EITHER(SPINDLE_FEATURE, LASER_FEATURE)
   #define SPINDLE_LASER_ACTIVE_HIGH     true   // Set to "true" if the on/off function is active HIGH
 #ifdef MINI_MODEL_2AM
