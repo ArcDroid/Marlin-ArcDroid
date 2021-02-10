08b0fd4cf12fa6422152c2684f9410910ab19170
Igor Yeremin <igor.yeremin@gmail.com>
2021-02-10 15:34:27 -0500

    Revert "Test sensorless homing", not using it on PPR now
    This reverts commit d14d34add2b251255a0e15d2e275eb2fda818537.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -713,11 +713,11 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-#define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+#define X_MIN_ENDSTOP_INVERTING true  // Set to true to invert the logic of the endstop.
+#define Y_MIN_ENDSTOP_INVERTING true  // Set to true to invert the logic of the endstop.
 #define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+#define X_MAX_ENDSTOP_INVERTING true  // Set to true to invert the logic of the endstop.
+#define Y_MAX_ENDSTOP_INVERTING true  // Set to true to invert the logic of the endstop.
 #define Z_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe.
 
@@ -777,7 +777,7 @@
  *
  * :[2,3,4,5,6,7]
  */
-////#define ENDSTOP_NOISE_THRESHOLD 3
+#define ENDSTOP_NOISE_THRESHOLD 3
 
 //=============================================================================
 //============================== Movement Settings ============================
